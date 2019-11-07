d47263b289bf5506f091a9335bd08d664c13b64a
dhiraj-githubac <dhiraj.kumar@smscountry.com>
2019-11-07 06:25:52 +0530

    Initial Commit

---
+++ order-management-frontend/src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+    {
+        _id: 0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id: 1,
+        name: "Rs.0 to Rs.10000",
+        array: [0, 10000]
+    },
+    {
+        _id: 2,
+        name: "Rs.11000 to Rs.20000",
+        array: [11000, 20000]
+    },
+    {
+        _id: 3,
+        name: "Rs.21000 to Rs.30000",
+        array: [21000, 30000]
+    },
+    {
+        _id: 4,
+        name: "Rs.31000 to Rs.40000",
+        array: [31000, 40000]
+    },
+    {
+        _id: 5,
+        name: "More than Rs.40000",
+        array: [40000, 100000]
+    }
+];
